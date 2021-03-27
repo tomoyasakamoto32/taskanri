10fb4d43b7572d1db9a0fc3925fe1cc350409313
tomoyasakamoto32 <oregairu0302@gmail.com>
2021-03-27 21:26:58 +0900

    create view about TaskController@index

---
+++ laravel/resources/views/tasks/index.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+  <div class="row">
+    <div class="col">
+      <a class="btn btn-primary btn-lg btn-block" href='#'>課題を登録する</a>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col">
+      <div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">近世中国史</h5>
+          <h6 class="card-subtitle mb-2 text-muted">レポート</h6>
+          <h6 class="card-subtitle mb-2 text-muted">3/24 2限終了時まで</h6>
+          <a href="#" class="card-link">詳細</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -17,6 +17,12 @@ Route::get('/', function () {
     return view('top');
 });
 
+Route::group(['prefix' => 'tasks', 'middleware' => 'auth'], function(){
+    Route::get('index', 'TaskController@index')->name('index');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
